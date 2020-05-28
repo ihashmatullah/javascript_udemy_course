8b538b6803be37493ef76a66c8732693c18f1c38
ihashmatullah <hashnoorzai@yahoo.com>
2020-05-28 15:13:22 -0500

    Sec7Lec118 Coding Challenge 8 Soultion

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -587,9 +587,158 @@ johnAthlete6.wonMedal();
 johnAthlete6.calculateAge();
 
 */
+/*
+// Coding challenge 8 
+console.log('----PARKS REPORT----');
+
+console.log('----STREETS REPORT----');
+
+//1. Tree density  of each park in  the town
+    //Forumula: number of trees/park area
+//Green Park has a tree density of 1075 trees per square km
+//National Park has a tree density of 1221.0344 trees per square km
+// Oak Park has a tree density of 2372.5 trees per square km 
+
+//2. Average age of each town's park (formula: sum of all ages/number of parks)
+//Our 3 parks have an average age of 71.33333 years.
+
+// Variable & Data structures
+const parksAge = new Map();
+        parksAge.set('greenPark', 80);
+        parksAge.set('nationalPark', 75);
+        parksAge.set('oakPark', 67);
+
+let sumOfAges = parksAge.get('greenPark') + parksAge.get('nationalPark') + parksAge.get('oakPark');
+
+let noOfParks = 3;
+
+// Classes
+class ParksReport {
+    constructor (parkName, noOfTrees, parkArea) {
+        this.parkName = parkName;
+        this.noOfTrees = noOfTrees;
+        this.parkArea = parkArea;
+    }
+    
+    calcTreeDensity() {
+        var treeDensity = this.noOfTrees /  this.parkArea;
+        console.log(`${this.parkName} has a tree density of ${treeDensity} trees per square km`);
+    } 
+}
+
+// Class Objects Instantiation
+const greenPark = new ParksReport('Green Park', 1900, 2);
+
+//const greenPark = new ParksReport('National Park', 2000, 3);
 
 
+// Object calls
+calcParksAvgAge();
+greenPark.calcTreeDensity();
 
+// Helper methods
+function calcParksAvgAge(sumOfAges, noOfParks) {
+    let avg = sumOfAges / noOfParks;
+    console.log('Our ' + noOfParks + ' parks have an average age of ' + avg + ' years.');
+}
+//3. The name of the park that has more than 1000 trees
+// National Park has more than 1000 trees.
+console.log('National Park has more than 1000 trees');
+
+//4. Total and average length of the town's streets
+//Our 4 streets have a total length of 7.1000 km, with an average of 1.77500 km.
+
+//5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown, the default is normal
+//Ocean Avenue, built in 1999, is a big stree.
+//Evergreen Street, built in 2000, is a smal street.
+//4th Street. build in 2015, is a normal street.
+//Sunset Boulevard, built in 1982, is a huge street.
+
+*/
+// Super class
+class Element {
+    constructor(name, buildYear) {
+        this.name = name;
+        this.buildYear = buildYear;
+    }
+}
+
+class Park extends Element {
+    constructor(name, buildYear, area, numTrees) {
+        super(name, buildYear);
+        this.area = area; //km2
+        this.numTrees = numTrees;
+    }
+    
+    treeDensity() {
+        const density = this.numTrees / this.area;
+        console.log(`${this.name} has a tree densityy of ${density} trees per square km.`);
+    }
+}
+
+class Street extends Element {
+    constructor(name, buildYear, length, size = 3) {
+        super(name, buildYear);
+        this.length = length;
+        this.size = size;
+    }
+    
+    classifyStreet () {
+        const classification = new Map();
+        classification.set(1, 'tiny');
+        classification.set(2, 'small');
+        classification.set(3, 'normal');
+        classification.set(4, 'big');
+        classification.set(5, 'huge');
+        console.log(`${this.name}, build in ${this.buildYear}, is a ${classification.get(this.size)} street.`);
+    }
+}
+
+
+const allParks = [new Park('Green Park', 1987, 0.2, 215),
+                 new Park('National Park',1894, 2.9, 3541), 
+                 new Park('Oak Park', 1953, 0.4, 949)];
+const allStreets = [new Street('Ocean Avenue', 1999, 1.1, 4), new Street('Evergreen Street', 2008, 2.7, 2), new Street('4th Street', 2015, 0.8), new Street('Sunset Boulevard', 1982, 2.5, 5)];
+
+function calc(arr) {
+    //reduce is used to reduce array to single value -- a ES5 method.
+    const sum = arr.reduce((prev, cur, index) => prev + cur, 0);
+    
+    return [sum, sum / arr.length];
+}
+
+
+function reportParks(p) {
+    
+    console.log('-----PARKS REPORT------');
+    
+    // Density
+    p.forEach(el => el.treeDensity());
+    // Average age
+    const ages = p.map(el => new Date().getFullYear() - el.buildYear);
+    const [totalAge, avgAge] = calc(ages);
+    console.log(`Our ${p.length} parks have an average of ${avgAge} years.`)
+    // Which park has more than 1000 trees
+    const i = p.map(el => el.numTrees).findIndex(el => el >= 1000);
+    console.log(`${p[i].name} has more than 1000 trees`);
+    
+    
+}
+
+
+function reportStreets(s) {
+    
+    console.log('-----Streets REPORT------');
+    
+    //Total and average length of the town's streets
+    const [totalLength, avgLength] = calc(s.map(el => el.length));
+    console.log(`Our ${s.length} streets have total length of ${totalLength} km, with an average of ${avgLength} km.`)
+    //Classify sizes
+    s.forEach(el => el.classifyStreet());
+}
+
+reportParks(allParks);
+reportStreets(allStreets);
 
 
 
